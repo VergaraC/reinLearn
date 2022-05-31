34725f9cd7511cb72f5feafb3b7bac5110f0ba5d
Týr <VergaraC>
2022-05-31 16:33:19 -0300

    ;

---
+++ LunarLander/trainedDoubleLunarLander.py
@@ -0,0 +1,27 @@
+from tabnanny import verbose
+import gym
+from tensorflow import keras
+import numpy as np
+
+env = gym.make('LunarLander-v2').env
+state = env.reset()
+#model = keras.models.load_model('data/model_double_lunar_land', compile=False)
+#model = keras.models.load_model('data/model_double1000_lunar_land', compile=False)
+#model = keras.models.load_model('data/model_double1500_lunar_land', compile=False)
+model = keras.models.load_model('data/model_double600V2_lunar_land', compile=False)
+
+
+done = False
+rewards = 0
+steps = 0
+
+while not done and steps < 3000: # steps igual ao treinamento
+    Q_values = model.predict(state[np.newaxis], verbose = 0)
+    action = np.argmax(Q_values[0])
+    state, reward, done, info = env.step(action)
+    rewards += reward
+    env.render()
+    steps += 1
+
+print(f'Score = {rewards}')
+input('press a key...')
